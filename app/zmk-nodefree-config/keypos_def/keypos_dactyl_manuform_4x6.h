55453804f89d2c9f630f22f139c80fb9a12149d5
Per Sommer <persommer@mailbox.org>
2023-08-03 13:02:06 +0200

    added own shields

---
+++ app/zmk-nodefree-config/keypos_def/keypos_dactyl_manuform_4x6.h
@@ -0,0 +1,83 @@
+/*                                 ADVANTAGE 360 KEY MATRIX / LAYOUT MAPPING
+ ╭─────────────────────────┬────────────────────────╮ ╭─────────────────────────┬──────────────────────────╮
+ │  0   1   2   3   4   5  │  6   7   8   9  10  11 │ │ LN5 LN4 LN3 LN2 LN1 LN0 │  RN0 RN1 RN2 RN3 RN4 RN5 │
+ │ 12  13  14  15  16  17  │ 18  19  20  21  22  23 │ │ LT5 LT4 LT3 LT2 LT1 LT0 │  RT0 RT1 RT2 RT3 RT4 RT5 │
+ │ 24  25  26  27  28  29  │ 30  31  32  33  34  35 │ │ LM5 LM4 LM3 LM2 LM1 LM0 │  RM0 RM1 RM2 RM3 RM4 RM5 │
+ │         36  37          │         38  39         │ │         LB1 LB0         │          RB0 RB1         │
+ ╰───────────┬─────────────┼─────────────┬──────────╯ ╰──────────┬──────────────┼──────────────┬───────────╯
+             │  40  41  42 │ 43  44  45  │                       │  LH4 LH3 LH2 │  RH2 RH3 RH4 │
+             ╰──╮ 46  47   │   48  49 ╭──╯                       ╰──╮ LH1 LH0   │   LH0 LH1 ╭──╯
+                ╰──────────┴──────────╯                             ╰───────────┴───────────╯ */
+
+#pragma once
+
+// left-top row
+#define LN0 5
+#define LN1 4
+#define LN2 3
+#define LN3 2
+#define LN4 1
+#define LN5 0
+
+// right-top row
+#define RN0 6
+#define RN1 7
+#define RN2 8
+#define RN3 9
+#define RN4 10
+#define RN5 11
+
+// left-middle row
+#define LT0 17
+#define LT1 16
+#define LT2 15
+#define LT3 14
+#define LT4 13
+#define LT5 12
+
+// right-middle row
+#define RT0 18
+#define RT1 19
+#define RT2 20
+#define RT3 21
+#define RT4 22
+#define RT5 23
+
+// left-middle row
+#define LM0 29
+#define LM1 28
+#define LM2 27
+#define LM3 26
+#define LM4 25
+#define LM5 24
+
+// right-middle row
+#define RM0 30
+#define RM1 31
+#define RM2 32
+#define RM3 33
+#define RM4 34
+#define RM5 35
+
+// left-bottom row
+#define LB0 37
+#define LB1 36
+
+// right-bottom row
+#define RB0 38
+#define RB1 39
+
+// left thumb keys
+#define LH0 47
+#define LH1 46
+#define LH2 42
+#define LH3 41
+#define LH4 40
+
+// right thumb keys
+#define RH0 43
+#define RH1 44
+#define RH2 45
+#define RH3 48
+#define RH4 49
+
