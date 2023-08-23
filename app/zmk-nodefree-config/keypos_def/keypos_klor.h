8d36d7fdcdc091c4e33eb9cd6556bf39ef4f3a7c
Per Sommer <persommer@mailbox.org>
2023-08-23 13:47:43 +0200

    added own shields

--- app/zmk-nodefree-config/keypos_def/keypos_klor.h
+++ app/zmk-nodefree-config/keypos_def/keypos_klor.h
@@ -3,8 +3,8 @@
   ╭────────────────────────────┬────────────────────────────╮ ╭─────────────────────────────┬─────────────────────────────╮
   │      0   1   2   3   4     │      5   6   7   8  9      │ │     LT4 LT3 LT2 LT1 LT0     │     RT0 RT1 RT2 RT3 RT4     │
   │ 10  11  12  13  14  15     │     16  17  18  19  20  21 │ │ LM5 LM4 LM3 LM2 LM1 LM0     │     RM0 RM1 RM2 RM3 RM4 RM5 │
-  │ 22  23  24  25  26  27  28 │ 29  30  31  32  33  34  35 │ │ LB5 LB4 LB3 LB2 LB1 LB0 LEC │ REC RB0 RB1 RB2 RB3 RB4 RB5 │
-  ╰───────────╮ 36  37  38  39 │ 40  41  42  43 ╭───────────╯ ╰───────────╮ LH3 LH2 LH1 LH0 │ RH0 RH1 RH2 RH3 ╭───────────╯
+  │ 22  23  24  25  26  27  38 │ 43  28  29  30  31  32  33 │ │ LB5 LB4 LB3 LB2 LB1 LB0 LEC │ REC RB0 RB1 RB2 RB3 RB4 RB5 │
+  ╰───────────╮ 34  35  36  37 │ 39  40  41  42 ╭───────────╯ ╰───────────╮ LH3 LH2 LH1 LH0 │ RH0 RH1 RH2 RH3 ╭───────────╯
               ╰────────────────┴────────────────╯                         ╰─────────────────┴─────────────────╯             */
 
 #pragma once
@@ -35,7 +35,7 @@
 #define RM4 20
 #define RM5 21
 
-#define LEC 28  // left encoder key
+#define LEC 38  // left encoder key
 #define LB0 27  // left-bottom row
 #define LB1 26
 #define LB2 25
@@ -43,21 +43,21 @@
 #define LB4 23
 #define LB5 22
 
-#define REC 29  // right encoder key
-#define RB0 30  // right-bottom row
-#define RB1 31
-#define RB2 32
-#define RB3 33
-#define RB4 34
-#define RB5 35
+#define REC 43  // right encoder key
+#define RB0 28  // right-bottom row
+#define RB1 29
+#define RB2 30
+#define RB3 31
+#define RB4 32
+#define RB5 33
 
-#define LH0 39  // left thumb keys
-#define LH1 38
-#define LH2 37
-#define LH3 36
+#define LH0 40  // left thumb keys
+#define LH1 39
+#define LH2 38
+#define LH3 37
 
-#define RH0 40  // right thumb keys
-#define RH1 41
-#define RH2 42
-#define RH3 43
+#define RH0 39  // right thumb keys
+#define RH1 40
+#define RH2 41
+#define RH3 42
 
